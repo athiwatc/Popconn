a9f802a8c8fce4344583e3dca88c027764e8f5d8
Athiwat Chunlakhan <athiwatc@gmail.com>
2013-05-10 19:52:18 +0700

    FUCKING FACMENTTT"GLEWOPITGNWILETN LW#NTG LI#WJTG KNW#LKTVNW#LJ NGJKW#BGK#WG

--- Android/PopConn/src/com/saranomy/popconn/MainActivity.java
+++ Android/PopConn/src/com/saranomy/popconn/MainActivity.java
@@ -1,67 +1,43 @@
 package com.saranomy.popconn;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
-	private Button activity_main_facebook;
-	private Button activity_main_twitter;
-	private Button activity_main_instagram;
-	private Button activity_main_start;
+public class MainActivity extends FragmentActivity {
+
 	
 	MainFragment mainFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
-		syncViewById();
-		init();
+		
+		if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        mainFragment = new MainFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        mainFragment = (MainFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
+		
+
 	}
 
-	private void syncViewById() {
-		activity_main_facebook = (Button) findViewById(R.id.activity_main_facebook);
-		activity_main_twitter = (Button) findViewById(R.id.activity_main_twitter);
-		activity_main_instagram = (Button) findViewById(R.id.activity_main_instagram);
-		activity_main_start = (Button) findViewById(R.id.activity_main_start);
-	}
-
-	private void init() {
-		activity_main_facebook.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(getApplicationContext(),
-						LoginFacebookActivity.class));
-			}
-		});
-		activity_main_twitter.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(getApplicationContext(),
-						LoginTwitterActivity.class));
-			}
-		});
-		activity_main_instagram.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(getApplicationContext(),
-						LoginInstagramActivity.class));
-			}
-		});
-		activity_main_start.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				startActivity(new Intent(getApplicationContext(),
-						FeedActivity.class));
-			}
-		});
-	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- Android/PopConn/src/com/saranomy/popconn/MainFragment.java
+++ Android/PopConn/src/com/saranomy/popconn/MainFragment.java
@@ -3,20 +3,20 @@ package com.saranomy.popconn;
 import java.util.Arrays;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphObject;
 import com.facebook.widget.LoginButton;
 
 public class MainFragment extends Fragment {
@@ -99,34 +99,66 @@ public class MainFragment extends Fragment {
 		}
 	}
 
-	class FacebookGetRequest extends AsyncTask<Session, Void, Void> {
-
-		@Override
-		protected Void doInBackground(Session... params) {
-
-			Request r = new Request(params[0], "me/home");
-
-			Response res = Request.executeAndWait(r);
-
-			GraphObject go = res.getGraphObject();
-
-			// Log.i("result", go.toString()); //HERE
-			return null;
-		}
-
-	}
-
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.activity_main, container, false);
 
 		LoginButton authButton = (LoginButton) view
-				.findViewById(R.id.activity_main_facebook);
+				.findViewById(R.id.authButton);
 		authButton.setFragment(this);
 		authButton.setReadPermissions(Arrays.asList("read_stream"));
+		
+		syncViewById(view);
+		init();
 
 		return view;
+	}
+	
+	private Button activity_main_facebook;
+	private Button activity_main_twitter;
+	private Button activity_main_instagram;
+	private Button activity_main_start;
+	
+	private void syncViewById(View view) {
+		Log.e("HAHAHA", "FESFWEFWEfwefwef");
+		activity_main_facebook = (Button) view.findViewById(R.id.activity_main_facebook);
+		activity_main_twitter = (Button) view.findViewById(R.id.activity_main_twitter);
+		activity_main_instagram = (Button) view.findViewById(R.id.activity_main_instagram);
+		activity_main_start = (Button) view.findViewById(R.id.activity_main_start);
+	}
+
+	private void init() {
+		Log.e("TESTAAA", "ASDASDS");
+		activity_main_facebook.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getActivity().getApplicationContext(),
+						LoginFacebookActivity.class));
+			}
+		});
+		activity_main_twitter.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getActivity().getApplicationContext(),
+						LoginTwitterActivity.class));
+			}
+		});
+		activity_main_instagram.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getActivity().getApplicationContext(),
+						LoginInstagramActivity.class));
+			}
+		});
+		activity_main_start.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getActivity().getApplicationContext(),
+						FeedActivity.class));
+			}
+		});
 	}
 
 }
