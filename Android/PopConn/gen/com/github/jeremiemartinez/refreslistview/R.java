ee70728deae985c0474d08f6b5b7bcf85443095d
Saran Siripuekpong <anti7gn@gmail.com>
2013-05-10 19:54:24 +0700

    Add icon in Item

--- Android/PopConn/gen/com/github/jeremiemartinez/refreslistview/R.java
+++ Android/PopConn/gen/com/github/jeremiemartinez/refreslistview/R.java
@@ -8,31 +8,39 @@ package com.github.jeremiemartinez.refreslistview;
 
 public final class R {
 	public static final class id {
+<<<<<<< HEAD
 		public static final int progress = 0x7f040021;
 		public static final int comment = 0x7f040022;
 		public static final int date = 0x7f040023;
 		public static final int arrow = 0x7f040020;
 		public static final int header = 0x7f04001f;
+=======
+		public static final int progress = 0x7f060020;
+		public static final int comment = 0x7f060021;
+		public static final int date = 0x7f060022;
+		public static final int arrow = 0x7f06001f;
+		public static final int header = 0x7f06001e;
+>>>>>>> Add icon in Item
 	}
 	public static final class style {
-		public static final int refreshlistview_container = 0x7f080002;
-		public static final int refreshlistview_date = 0x7f080006;
-		public static final int refreshlistview_comment = 0x7f080005;
-		public static final int refreshlistview_arrow = 0x7f080003;
-		public static final int refreshlistview_progress = 0x7f080004;
-		public static final int refreshlistview_header = 0x7f080007;
+		public static final int refreshlistview_container = 0x7f050000;
+		public static final int refreshlistview_date = 0x7f050004;
+		public static final int refreshlistview_comment = 0x7f050003;
+		public static final int refreshlistview_arrow = 0x7f050001;
+		public static final int refreshlistview_progress = 0x7f050002;
+		public static final int refreshlistview_header = 0x7f050005;
 	}
 	public static final class string {
-		public static final int refreshlistview_no_update = 0x7f05001d;
-		public static final int refreshlistview_arrow = 0x7f050019;
-		public static final int refreshlistview_pulldown = 0x7f05001c;
-		public static final int refreshlistview_release = 0x7f05001b;
-		public static final int refreshlistview_updating = 0x7f05001a;
+		public static final int refreshlistview_no_update = 0x7f040004;
+		public static final int refreshlistview_arrow = 0x7f040000;
+		public static final int refreshlistview_pulldown = 0x7f040003;
+		public static final int refreshlistview_release = 0x7f040002;
+		public static final int refreshlistview_updating = 0x7f040001;
 	}
 	public static final class layout {
 		public static final int layout_refreshlistview_header = 0x7f03000f;
 	}
 	public static final class drawable {
-		public static final int refreshlistview_arrow = 0x7f020024;
+		public static final int refreshlistview_arrow = 0x7f020026;
 	}
 }
