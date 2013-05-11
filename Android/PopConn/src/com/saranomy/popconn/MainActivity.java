10958ac3bc6e081b9d0fb1006f94a1042a8be2c2
Saran Siripuekpong <anti7gn@gmail.com>
2013-05-11 11:44:33 +0700

    Final!

--- Android/PopConn/src/com/saranomy/popconn/MainActivity.java
+++ Android/PopConn/src/com/saranomy/popconn/MainActivity.java
@@ -1,15 +1,19 @@
 package com.saranomy.popconn;
 
-import com.saranomy.popconn.core.FacebookCore;
-import com.saranomy.popconn.core.InstagramCore;
-import com.saranomy.popconn.core.TwitterCore;
-import com.saranomy.popconn.debug.KeyHash;
-
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.saranomy.popconn.core.InstagramCore;
+import com.saranomy.popconn.core.TwitterCore;
+import com.saranomy.popconn.debug.KeyHash;
 
 public class MainActivity extends FragmentActivity {
 
@@ -36,6 +40,7 @@ public class MainActivity extends FragmentActivity {
 		super.onResume();
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onMenuItemSelected(int featureId, MenuItem item) {
 		switch (item.getItemId()) {
@@ -48,6 +53,30 @@ public class MainActivity extends FragmentActivity {
 			mainFragment.updateButton();
 			Toast.makeText(getApplicationContext(), "Data cleared.", Toast.LENGTH_SHORT).show();
 			break;
+		case (R.id.menu_about):
+			AlertDialog di = new AlertDialog.Builder(MainActivity.this).create();
+			di.setIcon(R.drawable.ic_launcher);
+			di.setCancelable(false);
+			di.setTitle("About");
+			di.setMessage("made by Saranomy.");
+			di.setButton3("More", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					try {
+						Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("market://search?q=pub:Saranomy"));
+						startActivity(intent);
+					} catch (ActivityNotFoundException anfe) {
+						Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://play.google.com/store/apps/developer?id=Saranomy"));
+						startActivity(intent);
+					}
+				}
+			});
+			di.setButton2("Close", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+			});
+			di.show();
 		}
 		return true;
 	}
